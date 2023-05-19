e656fb26e09e9abff016844434b320e60808fc94
ajovicic019 <ajovicic019@gmail.com>
2023-05-19 10:23:34 +0200

    Added link to the GitHub repository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <footer>
+        This project was coded by Andrea Jovicic and it is{" "}
+        <a
+          href="https://github.com/ajovicic019/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          open sourced
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
