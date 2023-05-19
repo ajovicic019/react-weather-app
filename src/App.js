716b0300af4786ada5014a2664e87a16c43bb5ba
ajovicic019 <ajovicic019@gmail.com>
2023-05-19 10:30:31 +0200

    Installed bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <footer>
-        This project was coded by Andrea Jovicic and it is{" "}
-        <a
-          href="https://github.com/ajovicic019/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          open sourced
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <footer>
+          This project was coded by Andrea Jovicic and it is{" "}
+          <a
+            href="https://github.com/ajovicic019/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            open sourced
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
