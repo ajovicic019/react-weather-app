cdd3d576bb73a628f7f08a8948f84c10c4664dd3
ajovicic019 <ajovicic019@gmail.com>
2023-05-22 12:02:14 +0200

    Updated Weather Icon

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       city: response.data.name,
       humidity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      icon: `https://ssl.gstatic.com/onebox/weather/64/cloudy.png`,
+      icon: response.data.weather[0].icon,
       date: new Date(response.data.dt * 1000),
       ready: true,
     });
